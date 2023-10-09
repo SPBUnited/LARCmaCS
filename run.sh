97e0a4a5e358b94b2970a205cfc3461dea432331
Mikhail Lipkovich <lipkovich.mikhail@gmail.com>
2023-10-09 04:39:23 +0300

    Logging of LARCmaCS output

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-#export LD_LIBRARY_PATH=/usr/local/MATLAB/R2021b/bin/glnxa64/:${LD_LIBRARY_PATH}
-#export PATH=/usr/local/MATLAB/R2021b/bin:${PATH}
-build/bin/LARCmaCS
+LOG_FILE=logs/larcmacs.log
+MAX_LOG_SIZE=10000000
+
+touch ${LOG_FILE}
+LOG_SIZE=$(wc -l < ${LOG_FILE})
+
+if [ ${LOG_SIZE} -ge ${MAX_LOG_SIZE} ]; then
+  mv ${LOG_FILE} ${LOG_FILE}.bak
+fi
+
+build/bin/LARCmaCS 2>&1 | tee -a ${LOG_FILE}
 
